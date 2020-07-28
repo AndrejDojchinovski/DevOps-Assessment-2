fe54167e41c144649016ecb65964abed15ace553
AndrejDojchinovski <60952483+AndrejDojchinovski@users.noreply.github.com>
2020-07-28 18:48:37 +1000

    Unit tests

---
+++ util.test.js
@@ -0,0 +1,33 @@
+// importing generateText function into util.test.js file
+const { generateText, validateInput, createElement } = require('./util');
+
+ 
+test('should return false on empty text', () => {
+    const text = validateInput('');
+    expect(text).toBe(false);
+});
+
+test('should output name and age', () => {
+    const notEmpty = validateInput('   ', true, false);
+    expect(notEmpty).toBe(false);
+});
+
+test('should element be validated  ', () => {
+    createElement("div","string1","string2");
+    const element = document.querySelector('.string2');
+    expect(element).toBeDefined();
+});
+
+test('should element be validated ', () => {
+    createElement("div","name","name2");
+    const element = document.querySelector('.name2');
+    expect(element).toBeUndefined();
+});
+
+test('should output no data  ', () => {
+    const text = generateText('', null);
+    expect(text).toBe('');
+});
+
+
+
